7de3918569cff98c2fb7c0ece99eea8ad7550928
max44910 <s.l.quinn@hotmail.com>
2025-09-11 20:34:01 +0100

    Add files via upload

---
+++ file3.py
@@ -0,0 +1,10 @@
+import math
+
+def calculate_circumference(radius):
+  """
+  Calculates the circumference of a circle given its radius.
+  """
+  return 2 * math.pi * radius
+
+circle_radius = 5
+circumference_result = calculate_circumference(circle_radius)
